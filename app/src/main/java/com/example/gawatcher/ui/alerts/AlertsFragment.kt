e332fb7373040cdb1c06447e99e9c27cbf8228a7
gendy <mostafa.elgendy76@gmail.com>
2025-05-20 16:18:37 +0300

    UI-Updated

--- app/src/main/java/com/example/gawatcher/ui/alerts/AlertsFragment.kt
+++ app/src/main/java/com/example/gawatcher/ui/alerts/AlertsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.gawatcher.ui.gallery
+package com.example.gawatcher.ui.alerts
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.gawatcher.databinding.FragmentGalleryBinding
+import com.example.gawatcher.databinding.FragmentAlertsBinding
 
-class GalleryFragment : Fragment() {
+class AlertsFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentAlertsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val settingsViewModel =
+            ViewModelProvider(this).get(AlertsViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAlertsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textAlerts
+        settingsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/gawatcher/ui/alerts/AlertsViewModel.kt
+++ app/src/main/java/com/example/gawatcher/ui/alerts/AlertsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.gawatcher.ui.gallery
+package com.example.gawatcher.ui.alerts
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class AlertsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is Alerts Fragment"
     }
     val text: LiveData<String> = _text
 }
